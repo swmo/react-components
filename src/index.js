931af7c243bebeda8a51a9710ccee8894a7ae8c3
Moses Tschanz <moses.tschanz@gmail.com>
2019-10-27 17:47:24 +0100

    test with components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
-        <div> hi teher</div>
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Yesterday" text="fjsdla" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="2 min ago" text="fjdsafjal" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4pm" text="here some text" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+        </div>
     );
 }
 
